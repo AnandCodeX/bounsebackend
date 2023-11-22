8ea88b2fe3d999ed3237e3efb3ece15292b8e497
AnandCodeX <Anandrtiwari@live.com>
2023-11-22 12:11:39 +0000

    final

--- routes/countries.js
+++ routes/countries.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+//Get API Takes Country Name Fetch The Data From Given API Return The Response Data
 router.get("/country/:name", async (req, res) => {
 	console.log("API Hit");
 	const country = req.params.name;
@@ -12,16 +13,14 @@ router.get("/country/:name", async (req, res) => {
 	try {
 		const response = await axios.get(
 			`https://restcountries.com/v3.1/name/${country}`
-		);
-
+		)
+		
 		const countryInfo = response.data[0];
 		res.json({
 			name: countryInfo.name.common,
 			capital: countryInfo.capital[0],
 			population: countryInfo.population,
 			flag: countryInfo.flag,
-			map: countryInfo.maps.googleMaps,
-			fullData: countryInfo,
 			officialName: countryInfo.name.official,
 			continents: countryInfo.continents,
 		});
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 /** @format */
 
+//Important Imports
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+//Cors for Cross Platform 
 app.use(cors());
 
+//Routes & Middleware as of We Dont Have Any Middleware
 app.use("/", require("./routes/countries"));
 
+//Server Running Port Decleration 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
